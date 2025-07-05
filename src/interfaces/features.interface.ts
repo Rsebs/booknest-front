995296022faa7156dcb0017e1237d76dfefedd3a
Rsebs <sebastianruizj2014@gmail.com>
2025-07-04 19:07:18 -0500

    Se crea el layout de la aplicación

---
+++ src/interfaces/features.interface.ts
@@ -0,0 +1,9 @@
+interface FeatureRoute {
+  name: string;
+}
+
+export interface Features {
+  name: string;
+  icon: string;
+  route: FeatureRoute;
+}
--- src/views/layouts/MainLayout.vue
+++ src/views/layouts/MainLayout.vue
@@ -1,7 +1,53 @@
 <template>
-  <div class="d-flex ga-5">
-    <v-btn color="primary" @click="$router.push({ name: 'home' })"> Home Page </v-btn>
-    <v-btn color="secondary" @click="$router.push({ name: 'about' })"> About Page </v-btn>
-  </div>
-  <RouterView />
+  <v-layout>
+    <v-navigation-drawer v-model="drawer" elevation="1">
+      <v-list nav>
+        <v-list-item
+          v-for="feature in features"
+          :key="feature.route.name"
+          :title="feature.name"
+          link
+          @click="$router.push(feature.route)"
+        >
+          <template #prepend>
+            <v-avatar>
+              <v-icon color="primary" :icon="feature.icon" />
+            </v-avatar>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar title="Book Nest" app elevation="1">
+      <template #prepend>
+        <v-btn icon="mdi-menu" @click="drawer = !drawer" />
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
+
+<script lang="ts" setup>
+import type { Features } from '@/interfaces/features.interface';
+const { mobile } = useDisplay();
+import { ref } from 'vue';
+import { useDisplay } from 'vuetify';
+
+const drawer = ref(!mobile.value);
+
+const features: Features[] = [
+  {
+    name: 'Home Page',
+    icon: 'mdi-home',
+    route: { name: 'home' },
+  },
+  {
+    name: 'About Page',
+    icon: 'mdi-information',
+    route: { name: 'about' },
+  },
+];
+</script>
