9d60761a2bf9de68ecb28a286a097e9c3e443673
Rsebs <sebastianruizj2014@gmail.com>
2025-07-12 22:42:04 -0500

    Se instala Pinia Persistant para guardar los estados de pinia en local/session storage

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue';
 
-import { createPinia } from 'pinia';
 import vuetify from './vuetify.conf';
+import pinia from './pinia.conf';
 import router from '@/router';
 
 export const registerPlugins = (app: App) => {
-  app.use(vuetify).use(createPinia()).use(router);
+  app.use(vuetify).use(pinia).use(router);
 };
